494a419b324848f58df3cc0d9f3a87fdd97e1482
mj <mjkim012345@gmail.com>
2017-07-07 16:42:02 +0900

    Agglomerative algorithm

--- data.py
+++ data.py
@@ -7,14 +7,20 @@ def load_coil20():
     labels = np.ones(72)
 
     for i in range(2,21):
-        labels = np.concatenate((labels,np.full(51,i)),axis=0)
+        labels = np.concatenate((labels,np.full(72,i)),axis=0)
 
     for i in range(1,21):
         for j in range(72):
             index = (i-1)*72+j
             data[index]=imread("./dataset/coil-20/obj" + str(i) + "__" + str(j) + ".png","L")
-
+    print("label : ", len(labels))
     return data, labels
+
+def load_MNIST_test():
+    pass
+
+def load_MNIST_train():
+    pass
 
 
 #data, labels = load_coil20()
